5108a86fe9e5c46ead585c796e6c750f98e763cf
Administrator <Administrator@304-18>
2022-12-20 17:16:03 +0900

    카트 기능

---
+++ src/main/java/com/green/nowon/domain/entity/CartGoodsEntityRepository.java
@@ -0,0 +1,13 @@
+package com.green.nowon.domain.entity;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartGoodsEntityRepository extends JpaRepository<CartGoodsEntity, Long> {
+
+	Optional<CartGoodsEntity> findByCartNoAndGoodsGno(long cartNo, long goodsGno);
+
+}
